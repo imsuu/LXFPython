9b1d1696ca32199b716168c5952c09ea2951f415
imsuu <943707709@qq.com>
2018-07-23 14:08:46 +0800

    property 一定要记得下划线！

--- property.py
+++ property.py
@@ -47,5 +47,31 @@ class Student(object):
 
 	@property
 	def age(self):
-		return self._age
+		return 2018 - self._birth
 	
+class Screen(object):
+	"""docstring for Screen"""
+	@property
+	def width(self):
+		return self._width
+
+	@width.setter
+	def width(self,value):
+		self._width = value
+
+	@property
+	def height(self):
+		return self._height
+
+	@height.setter
+	def height(self,value):
+		self._height = value
+	
+	@property
+	def resolution(self):
+		return self._height * self._width
+
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution = ',s.resolution)
